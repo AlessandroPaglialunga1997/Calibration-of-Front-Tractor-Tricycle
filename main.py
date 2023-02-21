628e6bcb8946d3169b9045472cbbce905a41a159
AlessandroPaglialunga1997 <112383239+AlessandroPaglialunga1997@users.noreply.github.com>
2023-02-21 19:17:03 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from translate_time_stamp import * 
+from translate_timestamp import translate_timestamp
 from translate_incremental_encoder import *
-#translate_time_stamp()
-translate_incremental_encoder()
+number_of_comments_in_dataset_file = 8
+dataset_path = "dataset.txt"
+main_informations_separator = ":"
+translate_timestamp(dataset_path, number_of_comments_in_dataset_file, main_informations_separator)
+#translate_incremental_encoder()
