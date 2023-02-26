5e03e7eaf61b323df3ccec8a3132fdcb91aef0b2
AlessandroPaglialunga1997 <112383239+AlessandroPaglialunga1997@users.noreply.github.com>
2023-02-26 09:40:09 +0100

    Add files via upload

--- handle_dataset.py
+++ handle_dataset.py
@@ -4,9 +4,35 @@
 from utilities import *
 import numpy as np
 
-def read_DS(DS_path, num_comments, separator, new_DS_path):
+def clean_dataset_from_unuseful_spaces(DS_path, num_comments, new_DS_path):
+    DS_file = open(DS_path)
+    new_DS_file = open(new_DS_path,'w')
+    lines = DS_file.read().splitlines()
+    for line_idx in range(num_comments, len(lines)):
+        there_was_a_space = False
+        char_idx = 0
+        while char_idx < len(lines[line_idx]):
+            if(lines[line_idx][char_idx] == " "):
+                if(there_was_a_space):
+                    lines[line_idx] = lines[line_idx][:char_idx] + lines[line_idx][char_idx+1:]
+                    char_idx -= 1
+                else:
+                    there_was_a_space = True
+            else:
+                there_was_a_space = False
+            char_idx += 1
+        new_DS_file.write(lines[line_idx]+'\n')
+
+def read_DS(DS_path, num_comments, separator):
     num_records = num_of_lines_in_file(DS_path) - num_comments
     num_tokens = num_of_tokens_in_line(DS_path, num_comments, separator) - 1 #last one contains only numbers
+    DS_file = open(DS_path)
+    [num_infos_per_token_array, total_num_of_infos] = tokens_dimensions(DS_path, separator, num_comments, num_tokens)
+
+    data = take_data(DS_path, separator, num_records, num_comments, num_infos_per_token_array, total_num_of_infos)
+    return data
+
+def tokens_dimensions(DS_path, separator, num_comments, num_tokens):
     token_name_array = []
     num_infos_per_token_array = {}
     total_num_of_infos = 0
@@ -21,6 +47,11 @@ def read_DS(DS_path, num_comments, separator, new_DS_path):
         num_infos_per_token_array[token_name] = [num_infos, total_num_of_infos]
         total_num_of_infos += num_infos
     
+    return [num_infos_per_token_array, total_num_of_infos]
+
+def take_data(DS_path, separator, num_records, num_comments, num_infos_per_token_array, total_num_of_infos):
+    DS_file = open(DS_path)
+    lines = DS_file.read().splitlines()
     data = np.empty((num_records, total_num_of_infos), dtype='float64')
     for line_idx in range(num_comments, len(lines)):
         tokens = tokens_in_line(DS_path, line_idx, separator)
@@ -29,13 +60,6 @@ def read_DS(DS_path, num_comments, separator, new_DS_path):
             token_name = take_one_word_from_token(tokens[token_idx-1], -1)
             for info_idx in range(0, num_infos_per_token_array[token_name][0]):
                 column_idx = num_infos_per_token_array[token_name][1] + info_idx
-                print(column_idx)
                 data[record_idx][column_idx] = float(take_one_word_from_token(tokens[token_idx], info_idx))
-        print(record_idx)
-        
-    new_DS_file = open(new_DS_path, 'w')
-    for row in range(0, num_records):
-        for column in range(0, total_num_of_infos):
-            new_DS_file.write(str(data[row][column])+" ")
-        new_DS_file.write("\n")
-        
+    return data
+    
--- main.py
+++ main.py
@@ -11,7 +11,9 @@ import numpy as np
 
 DS_path = "dataset.txt"
 new_DS_path = "new_dataset.txt"
-num_comments = 8 # in the given dataset
+clened_DS_from_spaces_path = "clened_dataset_from_spaces.txt"
+num_comments_in_DS = 8 # in the given dataset
+num_comments_in_clened_DS = 0
 tokens_separator = ":"
 
 #[translated_TS_array, FIRST_TS] = translate_TS(DS_path, num_comments, tokens_separator)
@@ -20,4 +22,7 @@ tokens_separator = ":"
 #TS_val = val_TS_translation(DS_path, num_comments, tokens_separator, translated_TS_array, FIRST_TS)
 #IncEncInfo_val = val_IncEncInfo_translation(DS_path, num_comments, tokens_separator, translated_IncEncInfo_array, FIRST_IncEncInfo)
 
-read_DS(DS_path, num_comments, tokens_separator, new_DS_path)
+
+clean_dataset_from_unuseful_spaces(DS_path, num_comments_in_DS, clened_DS_from_spaces_path)
+data = read_DS(clened_DS_from_spaces_path, num_comments_in_clened_DS, tokens_separator, new_DS_path)
+
