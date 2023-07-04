55dacc8e7a7a38eb313922c7bbb8647abd0e6490
AlessandroPaglialunga1997 <112383239+AlessandroPaglialunga1997@users.noreply.github.com>
2023-07-04 20:17:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from dataset_handler import *
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 #-------------------------Create Clean and Consistent Dataset----------------------------
 
@@ -30,22 +32,22 @@ def read_from_consistent_dataset(consistent_dataset_path, info_separator, timest
         index_token = 0
         for token in all_tokens:
             if token.lower() == timestamp_name + info_separator:
-                timestamp.append(all_tokens[index_token + 1])
+                timestamp.append(float(all_tokens[index_token + 1]))
             elif token.lower() == encoders_values_name + info_separator:
-                abs_enc_value = all_tokens[index_token + 1]
-                inc_enc_value = all_tokens[index_token + 2]
+                abs_enc_value = int(all_tokens[index_token + 1])
+                inc_enc_value = int(all_tokens[index_token + 2])
                 curr_encoders_value_np = np.array([abs_enc_value, inc_enc_value])
                 encoders_values.append(curr_encoders_value_np)
             elif token.lower() == robot_confg_name + info_separator:
-                x = all_tokens[index_token + 1]
-                y = all_tokens[index_token + 2]
-                theta = all_tokens[index_token + 3]
+                x = float(all_tokens[index_token + 1])
+                y = float(all_tokens[index_token + 2])
+                theta = float(all_tokens[index_token + 3])
                 curr_robot_confg_np = np.array([x, y, theta])
                 robot_confg.append(curr_robot_confg_np)
             elif token.lower() == sensor_confg_name + info_separator:
-                x = all_tokens[index_token + 1]
-                y = all_tokens[index_token + 2]
-                theta = all_tokens[index_token + 3]
+                x = float(all_tokens[index_token + 1])
+                y = float(all_tokens[index_token + 2])
+                theta = float(all_tokens[index_token + 3])
                 curr_sensor_confg_np = np.array([x, y, theta])
                 sensor_confg.append(curr_sensor_confg_np)
             index_token += 1
@@ -63,6 +65,34 @@ def dimensions_sanity_checks(num_records, num_encoders, dim_robot_confg_space, d
     assert sensor_confg.shape[1] == dim_sensor_confg_space
 
 #--------------------------------------------------------------------------------------------
+def compute_trajectory(initial_state, parameters, sensor_confg):
+    trajectory = []
+    trajectory.append(initial_state)
+    curr_state = initial_state
+    for i in range(1, len(sensor_confg)):
+        delta_inc_enc = sensor_confg[i, 1] - sensor_confg[i-1, 1]
+        #print([sensor_confg[i, 1], sensor_confg[i-1, 1], delta_inc_enc, i])
+        curr_abs_enc = sensor_confg[i, 0]
+        next_state = prediction(parameters, curr_state, delta_inc_enc, curr_abs_enc)
+        trajectory.append(next_state)
+        curr_state = next_state
+    return np.array(trajectory)
+
+#--------------------------------------------------------------------------------------------
+
+def prediction(parameters, curr_state, delta_inc_enc, curr_abs_enc):
+    Ks, Kt, axis_length, steer_off = [parameters[0], parameters[1], parameters[2], parameters[3]]
+    curr_theta, curr_phi = [curr_state[2], curr_state[3]]
+    cos_theta_phi = math.cos(curr_theta + curr_phi)
+    sin_theta_phi = math.sin(curr_theta + curr_phi)
+    sin_phi = math.sin(curr_phi)
+    new_x = curr_state[0] + cos_theta_phi*Kt*delta_inc_enc
+    new_y = curr_state[1] + sin_theta_phi*Kt*delta_inc_enc
+    new_theta = curr_state[2] + sin_phi*Kt*delta_inc_enc/axis_length
+    new_phi = math.pi*Ks*curr_abs_enc/180 - math.pi*steer_off/180
+    return np.array([new_x, new_y, new_theta, new_phi])
+
+#--------------------------------------------------------------------------------------------
 
 info_separator = ":"
 timestamp_name = "time"
@@ -76,3 +106,14 @@ consistent_dataset_path = "Datasets/consistent_dataset.txt"
 
 [num_records, timestamp, encoders_values, robot_confg, sensor_confg] = read_from_consistent_dataset(consistent_dataset_path, info_separator, timestamp_name, encoders_values_name, robot_confg_name, sensor_confg_name)
 dimensions_sanity_checks(num_records, num_encoders, dim_robot_confg_space, dim_sensor_confg_space, timestamp, encoders_values, robot_confg, sensor_confg)
+initial_guess = np.array([360/8192, 0.0106141, 1.4, 0]) #[Ksteer, Ktraction, axis_length, steer_offset]
+initial_x = 0
+initial_y = 0
+initial_theta = 0
+initial_phi = math.pi*initial_guess[0]*sensor_confg[0,0]/180 - math.pi*initial_guess[3]/180
+initial_robot_confg = np.array([initial_x, initial_y, initial_theta, initial_phi])
+trajectory = compute_trajectory(initial_robot_confg, initial_guess, encoders_values)
+
+plt.plot(trajectory[:, 0], trajectory[:, 1])
+plt.plot(robot_confg[:, 0], robot_confg[:, 1])
+plt.show()
