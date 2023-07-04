e93807fc5b6ad3c0af320befceb81f3eabc20599
AlessandroPaglialunga1997 <112383239+AlessandroPaglialunga1997@users.noreply.github.com>
2023-07-04 15:34:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dataset_handler import *
-import decimal
+import numpy as np
+
+#-------------------------Create Clean and Consistent Dataset----------------------------
 
 # remove_unnecessary_comments_and_spaces(old_dataset_path = "Datasets/dataset.txt", #original dataset
 #                                        new_dataset_path = "Datasets/clened_dataset.txt", #dataset without comments and unnecessary spaces
 #                                        comment_symbol = "#") #if a line starts with this symbol then it is a comment
-# make_dataset_consistent(old_dataset_path = "Datasets/clened_dataset.txt", #cleaned dataset
+# make_dataset_consistent(old_dataset_path = "Datasets/clened_dataset.txt", #clean dataset
 #                         new_dataset_path = "Datasets/consistent_dataset.txt", #consistent dataset path
 #                         info_separator = ":", #each vector (info) is preceded by a name and a separator (e.g. time: ':' is the separator)
 #                         timestamp_name = "time",
@@ -12,3 +14,65 @@ import decimal
 #                         first_timestamp = 1668091584.821040869,
 #                         first_inc_enc = 4294859756,
 #                         overflow_value = 4294967295) # maximum value for incremental encoder
+
+#--------------------------------------------------------------------------------------------
+
+def read_from_consistent_dataset(consistent_dataset_path, info_separator, timestamp_name, encoders_values_name, robot_confg_name, sensor_confg_name):
+    timestamp = []
+    encoders_values = []
+    robot_confg = []
+    sensor_confg = []
+    consistent_dataset_file = open(consistent_dataset_path)
+    all_lines = consistent_dataset_file.read().splitlines()
+    num_records = len(all_lines)
+    for line in all_lines:
+        all_tokens = line.split(" ")
+        index_token = 0
+        for token in all_tokens:
+            if token.lower() == timestamp_name + info_separator:
+                timestamp.append(all_tokens[index_token + 1])
+            elif token.lower() == encoders_values_name + info_separator:
+                abs_enc_value = all_tokens[index_token + 1]
+                inc_enc_value = all_tokens[index_token + 2]
+                curr_encoders_value_np = np.array([abs_enc_value, inc_enc_value])
+                encoders_values.append(curr_encoders_value_np)
+            elif token.lower() == robot_confg_name + info_separator:
+                x = all_tokens[index_token + 1]
+                y = all_tokens[index_token + 2]
+                theta = all_tokens[index_token + 3]
+                curr_robot_confg_np = np.array([x, y, theta])
+                robot_confg.append(curr_robot_confg_np)
+            elif token.lower() == sensor_confg_name + info_separator:
+                x = all_tokens[index_token + 1]
+                y = all_tokens[index_token + 2]
+                theta = all_tokens[index_token + 3]
+                curr_sensor_confg_np = np.array([x, y, theta])
+                sensor_confg.append(curr_sensor_confg_np)
+            index_token += 1
+    return [num_records, np.array(timestamp), np.array(encoders_values), np.array(robot_confg), np.array(sensor_confg)]
+
+#--------------------------------------------------------------------------------------------
+
+def dimensions_sanity_checks(num_records, num_encoders, dim_robot_confg_space, dim_sensor_confg_space, timestamp, encoders_values, robot_confg, sensor_confg):
+    assert timestamp.shape[0] == num_records
+    assert encoders_values.shape[0] == num_records
+    assert encoders_values.shape[1] == num_encoders
+    assert robot_confg.shape[0] == num_records
+    assert robot_confg.shape[1] == dim_robot_confg_space
+    assert sensor_confg.shape[0] == num_records
+    assert sensor_confg.shape[1] == dim_sensor_confg_space
+
+#--------------------------------------------------------------------------------------------
+
+info_separator = ":"
+timestamp_name = "time"
+encoders_values_name = "ticks"
+num_encoders = 2
+robot_confg_name = "model_pose"
+dim_robot_confg_space = 3
+sensor_confg_name = "tracker_pose"
+dim_sensor_confg_space = 3
+consistent_dataset_path = "Datasets/consistent_dataset.txt"
+
+[num_records, timestamp, encoders_values, robot_confg, sensor_confg] = read_from_consistent_dataset(consistent_dataset_path, info_separator, timestamp_name, encoders_values_name, robot_confg_name, sensor_confg_name)
+dimensions_sanity_checks(num_records, num_encoders, dim_robot_confg_space, dim_sensor_confg_space, timestamp, encoders_values, robot_confg, sensor_confg)
